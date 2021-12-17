a2852139c0b5b01efc9790599dc3e7113c10598d
ZKL <1104569847@qq.com>
2021-12-16 19:17:28 -0800

    et

--- Chatroom/Chat_serv_epoll.cpp
+++ Chatroom/Chat_serv_epoll.cpp
@@ -10,11 +10,14 @@
 #include<errno.h>
 #include<fcntl.h>
 #include<sys/epoll.h>
+#include<vector>
+using namespace std;
 #define FD_LIMIT 65535
 #define BUFFER_SIZE 1024
 #define USER_LIMIT 5
 #define MAX_EVENT_NUMBER 1024
 struct client_data{
+	int fd;
 	struct sockaddr_in address;
 	char *write_buf;
 	char buf[BUFFER_SIZE];
@@ -27,16 +30,17 @@ int setnoblocking(int fd){
 
 }
 
-void addfd(int epollfd, int fd, bool enable_et){
+void addfd(int epollfd, int fd, bool enable_et, struct client_data *cld){
 	epoll_event event;
-	event.data.fd = fd;
-	event.events = EPOLLIN | EPOLLOUT | EPOLLRDHUP | EPOLLERR;
+	event.data.ptr = cld;
+	event.events = EPOLLIN | EPOLLRDHUP | EPOLLERR;
 	if(enable_et){
 		event.events |= EPOLLET;
 	}
 	epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &event);
 	setnoblocking(fd);
 }
+/*
 void lt(int number, struct epoll_event *events, int epollfd, int listenfd,int user_counter,struct client_data *users){
 		int ret = 0;
 		for(int i = 0; i < number; i++){
@@ -121,6 +125,116 @@ void lt(int number, struct epoll_event *events, int epollfd, int listenfd,int us
 
 
 }
+*/
+void et(int number, struct epoll_event *events, int epollfd, int listenfd,int user_counter,struct vector<client_data> &users){
+		int ret = 0;
+		for(int i = 0; i < number; i++){
+			client_data *scedp = static_cast<struct client_data*>(events[i].data.ptr);
+			//int sockfd = static_cast<struct client_data*>(events[i].data.ptr)->fd;
+			int sockfd = scedp->fd;
+			if(sockfd == listenfd){
+				struct sockaddr_in client_address;
+				socklen_t client_addrlength = sizeof(client_address);
+				int connfd = accept(listenfd, (struct sockaddr*)&client_address, &client_addrlength);
+
+				if(connfd < 0){
+					printf("errno is %d\n",errno);
+					continue;
+				}
+				//user_counter >= USER_LIMIT
+				if(users.size() >= USER_LIMIT){
+					const char *info = "too many users\n";
+					printf("%s\n",info);
+					send(connfd, info, strlen(info), 0);
+					continue;
+				}
+
+				struct client_data nclient;
+				nclient.address = client_address;
+				nclient.fd = connfd;
+				users.push_back(nclient);
+				//users[connfd].address = client_address;
+				addfd(epollfd, connfd, true, &users[users.size()-1]);
+				printf("comes a new users, now have %d users\n",user_counter);
+			
+			}
+			else if(events[i].events & EPOLLERR){
+				printf("get an error from %d\n",scedp->fd);
+				char errors[100];
+				memset(errors, '\0', 100);
+				socklen_t length = sizeof(errors);
+				if(getsockopt(scedp->fd, SOL_SOCKET, SO_ERROR, &errors, &length) < 0){
+					printf("get sockopt error\n");
+				}
+				continue;
+			}
+			else if(events[i].events & EPOLLRDHUP){
+				int index = scedp - &*(users.begin());
+				users[index] = users[users.size()-1];
+				users.pop_back();
+				//users[sockfd] = users[events[user_counter].data.fd];
+				//close(sockfd);
+				epoll_ctl(epollfd, EPOLL_CTL_DEL, sockfd, NULL);	
+				//user_counter--;
+				printf("a client left");
+			
+			}
+			else if(events[i].events & POLLIN){
+					struct epoll_event event;
+					event.data.ptr = scedp;
+					event.events = EPOLLOUT | EPOLLRDHUP | EPOLLERR;
+					epoll_ctl(epollfd, EPOLL_CTL_MOD, sockfd, &event);
+					while(1){
+						memset(scedp->buf,'\0',BUFFER_SIZE);
+						ret = recv(sockfd, scedp->buf, BUFFER_SIZE-1, 0);
+						printf("get %d bytes of client data %s from %d\n",ret,scedp->buf, sockfd);
+						if(ret < 0){
+							if(errno != EAGAIN){
+								close(sockfd);
+								//users[sockfd] = users[events[user_counter].data.fd];
+								int dex = scedp - &*(users.begin());
+								users.pop_back();
+								epoll_ctl(epollfd, EPOLL_CTL_DEL, sockfd, NULL);
+								//user_counter--;
+								break;
+							}
+							printf("read leater");
+							break;
+						}
+						else if(ret == 0){
+						}
+						else{
+							for(int j = 0; j < users.size(); ++j){
+								int jfd = users[j].fd;
+								if((jfd == sockfd ) || (jfd == listenfd))
+									continue;
+								//strcat(users[jfd].write_buf,users[sockfd].buf);
+								//users[jfd].write_buf = users[sockfd].buf;
+								users[j].write_buf = scedp->buf;
+							}
+						}
+					}
+			}	
+			else if(events[i].events & POLLOUT){
+					struct epoll_event event;
+					event.data.ptr = events[i].data.ptr;
+					event.events = EPOLLIN | EPOLLRDHUP | EPOLLERR;
+					epoll_ctl(epollfd, EPOLL_CTL_MOD, sockfd, &event);
+
+				if(scedp->write_buf == NULL){
+					continue;
+				}
+					ret = send(sockfd, scedp->write_buf, strlen(scedp->write_buf), 0);
+					//users[sockfd].write_buf = NULL;
+					scedp->write_buf = NULL;
+			
+			}
+
+		}
+}
+
+
+
 int main(int argc, char *argv[]){
 	if(argc <= 2){
 		printf("ipaddress, port");
@@ -144,16 +258,20 @@ int main(int argc, char *argv[]){
 	ret = listen(listenfd,5);
 	assert(ret != -1);
 
-	struct client_data *users = new client_data[FD_LIMIT];
+	//struct client_data *users = new client_data[FD_LIMIT];
+	vector<client_data> users;
 	int user_counter = 0;
 	
 	int epollfd = epoll_create(USER_LIMIT + 1);
 	assert(epollfd != -1);
-	struct epoll_event event;
-	event.data.fd = listenfd;
-	event.events = EPOLLIN | EPOLLERR | EPOLLET;
-	setnoblocking(listenfd);
-	epoll_ctl(epollfd, EPOLL_CTL_ADD, listenfd, &event );
+	//struct epoll_event even;
+	//event.data.fd = listenfd;
+	//event.events = EPOLLIN | EPOLLERR | EPOLLET;
+	//setnoblocking(listenfd);
+	//epoll_ctl(epollfd, EPOLL_CTL_ADD, listenfd, &event );
+	client_data lsen;
+	lsen.fd = listenfd;
+	addfd(epollfd, listenfd, true, &lsen);
 	struct epoll_event events[MAX_EVENT_NUMBER];
 	
 	while(1){
@@ -163,9 +281,8 @@ int main(int argc, char *argv[]){
 			printf("poll failure\n");
 			break;
 		}
-		lt(number, events, epollfd, listenfd,user_counter,users);
+		et(number, events, epollfd, listenfd,user_counter,users);
 	}
-	delete[]users;
 	close(listenfd);
 	return 0;
 }
